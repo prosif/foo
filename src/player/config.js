b1b9895eb08ee44a5d721835d4cf341476562d99
prosif <prosif@gmail.com>
2015-07-24 15:51:30 -0700

    added player config

---
+++ src/player/config.js
@@ -0,0 +1,15 @@
+define(function(require) {
+
+    var Config = {
+        Player: {     
+            size: {x: 20, y: 20},
+            center: {x:10, y: 10},
+            animations : [{        
+                name: "Walk_R",
+                frames: 7
+            }]
+        }
+    }
+
+    return Config;
+});
