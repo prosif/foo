5f49dd2fe37a0ad6beb53446a09787e76fb3929e
prosif <prosif@gmail.com>
2015-07-24 16:21:08 -0700

    changed initial pos of enemy

--- src/enemy/config.js
+++ src/enemy/config.js
@@ -3,7 +3,7 @@ define(function(require) {
     var Config = {
         Enemy: {     
             size: {x: 20, y: 20},
-            center: {x:10, y: 10},
+            center: {x:100, y: 100},
             animations : [{        
                 name: "Walk_R",
                 frames: 7
