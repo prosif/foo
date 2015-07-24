ba60c10e3026bb3568ff9112053e0e59b9058ae3
prosif <prosif@gmail.com>
2015-07-24 15:56:15 -0700

    add enemy config

---
+++ src/enemy/config.js
@@ -0,0 +1,15 @@
+define(function(require) {
+
+    var Config = {
+        Enemy: {     
+            size: {x: 20, y: 20},
+            center: {x:10, y: 10},
+            animations : [{        
+                name: "Walk_R",
+                frames: 7
+            }]
+        }
+    }
+
+    return Config;
+});
